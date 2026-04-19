eecc6457d0d4791e492c82601164fe28ec10966d
chugn <chungteocu@gmail.com>
2026-04-19 19:45:07 +0700

    Create TRR1_2011 - Xâu nhị phân 01.c

---
+++ TRR1_2011 - Xâu nhị phân 01.c
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    vector<long long> dp(m + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        dp[i] = 0;
+        for (int j = 1; j <= n; j++)
+        {
+            if (i - j >= 0)
+            {
+                dp[i] += dp[i - j];
+            }
+            else if (i - j == -1)
+            {
+                dp[i] += 1;
+            }
+        }
+    }
+    cout << dp[m];
+}
