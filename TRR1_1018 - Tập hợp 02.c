f66f403adaa3f6833181ad8a064b1043d3b05336
chugn <chungteocu@gmail.com>
2026-04-19 19:42:35 +0700

    Add program to find common elements in two arrays

---
+++ TRR1_1018 - Tập hợp 02.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    int a[1005], b[1005], p1 = 0, p2 = 0, th = 0;
+    int th1[1005], th2[1005], hop[1005];
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (a[i] == 1)
+        {
+            th1[p1++] = i + 1;
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (b[i] == 1)
+        {
+            th2[p2++] = i + 1;
+        }
+    }
+    int total = 0;
+    for (int i = 0; i < p2; i++)
+    {
+        int check = 0;
+        for (int j = 0; j < p1; j++)
+        {
+            if (th1[j] == th2[i])
+            {
+                check = 1;
+                break;
+            }
+        }
+        if (check == 1)
+        {
+            hop[th++] = th2[i];
+            total++;
+        }
+    }
+    for (int i = 0; i < total; i++)
+    {
+        for (int j = i + 1; j < total; j++)
+        {
+            if (hop[i] > hop[j])
+            {
+                int tmp2 = hop[i];
+                hop[i] = hop[j];
+                hop[j] = tmp2;
+            }
+        }
+    }
+    printf("%d\n", total);
+    for (int i = 0; i < total; i++)
+    {
+        printf("%d ", hop[i]);
+    }
+}
